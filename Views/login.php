8482eec2201b645f56541b6fdbb39899e17b2f12
oga1203 <86777014+oga1203@users.noreply.github.com>
2021-12-05 16:46:10 +0900

    バリデーション追加

--- Views/login.php
+++ Views/login.php
@@ -1,21 +1,37 @@
 <?php
-//追加
-if (isset($_POST['email']) && isset($_POST['password'])) {
-	require_once(ROOT_PATH . 'Controllers/UserController.php');
-	$users = new UserController();
-	$params = $users->check();
-	$user = $params['user'];
-	if (password_verify($_POST['password'], $user['password'])) {
-		session_start();
-		$_SESSION = $user;
-		//ログイン後、メイン画面へ遷移
-		header('Location: main.php');
-	} else {
-		$error_alert = "<script type='text/javascript'>
-		alert('パスワードが違います！');
-		location.href = 'login.php';
-		</script>";
-		echo $error_alert;
+$errors = [];
+if (isset($_POST)) {
+	// メールアドレスのチェック
+	if (empty($_POST['email'])) {
+		$errors['email'] = "メールアドレスは必須入力です。正しくご入力ください。";
+	} elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = "メールアドレスは正しくご入力ください。";
+	}
+	// パスワードのチェック
+	if (empty($_POST['password'])) {
+		$errors['password'] = "パスワードは必須入力です。正しくご入力ください。";
+	} elseif (!preg_match("/^[a-zA-Z0-9]+$/", $_POST['password'])) {
+		$errors['password'] = "パスワードは半角英数字のみでご入力ください。";
+	}
+	// エラーが空かどうかのチェック
+	if (empty($errors)) {
+		require_once(ROOT_PATH . 'Controllers/UserController.php');
+		$users = new UserController();
+		$params = $users->check();
+		$user = $params['user'];
+		// パスワードが合っているかどうかのチェック
+		if (password_verify($_POST['password'], $user['password'])) {
+			session_start();
+			$_SESSION = $user;
+			//ログイン後、メイン画面へ遷移
+			header('Location: main.php');
+		} else {
+			$error_alert = "<script type='text/javascript'>
+			alert('パスワードが違います！');
+			location.href = 'login.php';
+			</script>";
+			echo $error_alert;
+		}
 	}
 }
 ?>
@@ -40,9 +56,29 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 		<div class="login_body">
 			<form action="" method="post">
 				<p>メールアドレス</p>
-				<input type="text" name="email" placeholder="sample@sample.com" required>
+				<input type="text" name="email" placeholder="sample@sample.com">
+				<!-- エラーの際に表示 -->
+				<div class="error">
+					<p>
+						<?php
+						if (isset($_POST['email'])) {
+							echo $errors['email'];
+						}
+						?>
+					</p>
+				</div>
 				<p>パスワード</p>
-				<input type="password" name="password" placeholder="password" required>
+				<input type="password" name="password" placeholder="password">
+				<!-- エラーの際に表示 -->
+				<div class="error">
+					<p>
+						<?php
+						if (isset($_POST['password'])) {
+							echo $errors['password'];
+						}
+						?>
+					</p>
+				</div>
 				<div class="button">
 					<input type="submit" name="login" value="ログイン" class="submit" id="login" style="font-size:18px;">
 				</div>
