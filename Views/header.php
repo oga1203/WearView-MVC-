f7393ad838854db864bed4e47d27a25adbc87a8d
oga1203 <86777014+oga1203@users.noreply.github.com>
2021-12-18 22:01:44 +0900

    整理

--- Views/header.php
+++ Views/header.php
@@ -1,22 +1,3 @@
-<?php
-if (isset($_SESSION['role'])) {
-	$link = 'logout.php';
-	$link_name = 'ログアウト';
-	$mypage = $_SESSION['user_id'];
-	//管理者権限に分けて表示を選択
-	if ($_SESSION['role'] == 0) {
-		$role = null;
-	} else {
-		$role = 'none';
-	}
-} else {
-	$link = 'login.php';
-	$link_name = 'ログイン';
-	$mypage = null;
-	$table_class = 'none';
-	$role = 'none';
-}
-?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -31,15 +12,25 @@ if (isset($_SESSION['role'])) {
 	<div class="header">
 		<a href="main.php">Wear View</a>
 		<div class="">
-			<a href="manager_list.php" class="<?php echo $role; ?>">管理者一覧</a><!-- 管理者がログイン状態で表示 -->
+			<?php if (isset($_SESSION['user_id'])) : ?>
+				<!-- 管理者がログイン状態で表示 -->
+				<?php if ($_SESSION['role'] == 0) : ?>
+					<a href="manager_list.php" class="">管理者一覧</a>
+				<?php endif; ?>
+			<?php endif; ?>
 			<a href="category.php">カテゴリー検索</a>
 			<!-- 不要？検討中 -->
 			<!-- <a href="category_mid.php">中カテゴリー一覧</a> -->
 			<a href="brand.php">ブランド検索</a>
-			<a href="mypage.php?user_id=<?= $mypage ?>" class="<?php echo $table_class; ?>">マイページ</a>
+			<a href="search.php">検索</a>
 			<!-- 不要？検討中 -->
-			<!-- <a href=" search.php">検索</a> -->
-			<a href="<?php echo $link; ?>"><?php echo $link_name; ?></a><!-- ログイン状態で表示 -->
+			<!-- ログインまたはログアウト判定 -->
+			<?php if (isset($_SESSION['user_id'])) : ?>
+				<a href="mypage.php?user_id=<?= $_SESSION['user_id'] ?>" class="">マイページ</a>
+				<a href="logout.php">ログアウト</a>
+			<?php else : ?>
+				<a href="login.php">ログイン</a>
+			<?php endif; ?>
 		</div>
 	</div>
 </body>
